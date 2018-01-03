ac07d845feaff92d83e6115414ff2a31df51332f
Javier Buitrago <jbuitrago2014@fau.edu>
2018-01-02 20:30:08 -0500

    signin and signup authentication is working in the frontend and backend

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -15,6 +15,15 @@ export class AuthService {
         return this.http.post('http://localhost:3000/user', body, {headers: headers})
         .map((response: Response) => response.json())
         .catch((error: Response) => Observable.throw(error.json()));
-    
+
+  }
+
+  signin(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/user/signin', body, {headers: headers})
+        .map((response: Response) => response.json())
+        .catch((error: Response) => Observable.throw(error.json()));
+
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 var User = require('../models/user');
 
 
@@ -26,4 +27,34 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.post('/signin', function (req, res, next) {
+    User.findOne({email: req.body.email}, function (error, user) {
+      if (err) {
+          return res.status(500).json({
+              title: 'An error occurred',
+              error: err
+          });
+      }
+      if (!user) {
+        return res.status(401).json( {
+        title: 'Login Failed',
+        error: {message:'Invalid Logins Credentials'}
+
+        });
+      }
+      if (!bcrypt.compareSync(req.body.password,user.password)) {
+        return res.status(401).json( {
+        title: 'Login Failed',
+        error: {message:'Invalid Logins Credentials'}
+        });
+        }
+        var token = jwt.sign({user: user},'secret', {expiresIn: 7200 });
+        res.status(200).json({
+          message: 'Successfully logged in ',
+          token: token,
+          userId: user._id
+
+        });
+    });
+});
 module.exports = router;
